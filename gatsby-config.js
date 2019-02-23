9b6c9c8dddf8bf31fee5882e19e65c75b8875ffe
leny <info@flatland.be>
2019-02-23 14:23:30 +0100

    Import eslint configuration, import and setup emotion & koutla-swiss

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/* leny/kourikoulom
+ *
+ * /gatsby-config.js - Gatsby main configuration file
+ *
+ * coded by leny@flatLand!
+ * started at 23/02/2019
+ */
+
+module.exports = {
+    plugins: [
+        {
+            resolve: `gatsby-plugin-emotion`,
+        },
+    ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
-import React from "react"
+/* leny/kourikoulom
+ *
+ * /src/pages/index.js - Entry point page container
+ *
+ * coded by leny@flatLand!
+ * started at 23/02/2019
+ */
 
-export default () => <div>Hello world!</div>
+import React from "react";
+import {css} from "@emotion/core";
+import {rem} from "koutla-swiss";
+
+const styles = {
+    tmp: css({
+        fontSize: rem(4),
+        color: "red",
+    }),
+};
+
+export default () => <div css={styles.tmp}>{"Hey, leny!"}</div>;
