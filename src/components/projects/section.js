6c6078febf2b32a8b4b881498ac750eb41b354fa
leny <info@flatland.be>
2019-02-24 16:50:37 +0100

    Extract data from projects section

--- src/components/projects/section.js
+++ src/components/projects/section.js
@@ -7,34 +7,12 @@
  */
 
 import React from "react";
+import {StaticQuery, graphql} from "gatsby";
 import {css} from "@emotion/core";
 import {rem, margin} from "koutla-swiss";
 
 import Section from "../commons/section";
 import Entry from "./entry";
-
-// TODO: extract this
-
-const PROJECTS = [
-    {
-        name: "Kouto Swiss",
-        url: "http://kouto-swiss.io",
-        tooltip: "Visiter le site de Kouto Swiss",
-        description: "Framework Open-Source pour Stylus",
-    },
-    {
-        name: "Tèsèvè",
-        url: "http://teseve.github.io",
-        tooltip: "Visiter le site de Tèsèvè",
-        description: "Utilitaire Open-Source multiplateforme (Electron)",
-    },
-    {
-        name: "egzagò",
-        url: "http://egzago.com",
-        tooltip: "Visiter le site d'egzagò",
-        description: "Jeux Mobile (iOS / Swift)",
-    },
-];
 
 const styles = {
     list: css({
@@ -53,40 +31,63 @@ const styles = {
 };
 
 export default ({className}) => (
-    <Section className={className} title={"Projets personnels"} icon={"flask"}>
-        <dl css={styles.list}>
-            {PROJECTS.map(project => (
-                <Entry key={project.name} {...project} />
-            ))}
-            <dt css={styles.elementTitle}>{"et plein d'autres :"}</dt>
-            <dd>
-                <ul css={styles.sublist}>
-                    <li>
-                        <a
-                            href={"https://github.com/leny?tab=repositories"}
-                            rel={"external"}
-                            title={"Mes repositories sur GitHub"}>
-                            {"sur GitHub"}
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href={"https://www.npmjs.com/~leny"}
-                            rel={"external"}
-                            title={"Mes packages sur npm"}>
-                            {"sur npm"}
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href={"https://atom.io/users/leny/packages"}
-                            rel={"external"}
-                            title={"Mes packages pour Atom"}>
-                            {"sur Atom"}
-                        </a>
-                    </li>
-                </ul>
-            </dd>
-        </dl>
-    </Section>
+    <StaticQuery
+        query={graphql`
+            query {
+                allProjectsJson {
+                    edges {
+                        node {
+                            name
+                            url
+                            tooltip
+                            description
+                        }
+                    }
+                }
+            }
+        `}
+        render={data => (
+            <Section
+                className={className}
+                title={"Projets personnels"}
+                icon={"flask"}>
+                <dl css={styles.list}>
+                    {data.allProjectsJson.edges.map(({node: project}) => (
+                        <Entry key={project.name} {...project} />
+                    ))}
+                    <dt css={styles.elementTitle}>{"et plein d'autres :"}</dt>
+                    <dd>
+                        <ul css={styles.sublist}>
+                            <li>
+                                <a
+                                    href={
+                                        "https://github.com/leny?tab=repositories"
+                                    }
+                                    rel={"external"}
+                                    title={"Mes repositories sur GitHub"}>
+                                    {"sur GitHub"}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href={"https://www.npmjs.com/~leny"}
+                                    rel={"external"}
+                                    title={"Mes packages sur npm"}>
+                                    {"sur npm"}
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href={"https://atom.io/users/leny/packages"}
+                                    rel={"external"}
+                                    title={"Mes packages pour Atom"}>
+                                    {"sur Atom"}
+                                </a>
+                            </li>
+                        </ul>
+                    </dd>
+                </dl>
+            </Section>
+        )}
+    />
 );
