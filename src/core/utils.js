b154909441b447a9ad61eaef6053e18bf1959c62
leny <info@flatland.be>
2019-03-13 23:02:56 +0100

    Generate mq util

---
+++ src/core/utils.js
@@ -0,0 +1,11 @@
+/* leny/kourikoulom
+ *
+ * /src/core/utils.js - Misc utils
+ *
+ * coded by leny@flatLand!
+ * started at 13/03/2019
+ */
+
+export const mq = (query, props = {}) => ({
+    [`@media(${query})`]: props,
+});
