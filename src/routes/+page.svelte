4319c9e484ce8dbf48d022798b7a4a569cec8536
leny <info@flatland.be>
2026-01-04 14:36:41 +0100

    Link header components in page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <!-- @leny/kourikoulom - /src/routes/+page.svelte -->
 
+<script>
+    import { Header, ColorSchemeSwitcher } from "$lib/components/header";
+</script>
+
 <svelte:head>
     <title>Pierre-Antoine Delnatte - Web Developer</title>
 </svelte:head>
 
 <main>
-    <p>SvelteKit setup OK - Migration en cours...</p>
+    <Header />
+    <ColorSchemeSwitcher />
+    <p>Migration en cours...</p>
 </main>
